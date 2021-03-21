e3c4f7647256906470ad1831fff74ee3ebefac1e
authorblues <authorblues@gmail.com>
2021-03-20 21:46:31 -0400

    v1.0 release

---
+++ shuffler-src/setupform.lua
@@ -0,0 +1,87 @@
+function setup_form(callback)
+	local form, seed_text, min_text, max_text, resume, start_btn
+	local hk_complete
+
+	-- I believe none of these conflict with default hotkeys
+	local HOTKEY_OPTIONS = {
+		'Ctrl+Shift+End',
+		'Ctrl+Shift+Delete',
+		'Ctrl+Shift+D',
+		'Alt+Shift+End',
+		'Alt+Shift+Delete',
+		'Alt+Shift+D',
+	}
+
+	function start_handler()
+		if not forms.ischecked(resume) then
+			save_new_settings()
+		end
+
+		forms.destroy(form)
+		callback()
+	end
+
+	function save_new_settings()
+		config['seed'] = tonumber(forms.gettext(seed_text) or "0")
+		config['nseed'] = config['seed']
+
+		local a = tonumber(forms.gettext(min_text) or "15")
+		local b = tonumber(forms.gettext(max_text) or "45")
+		config['min_swap'] = math.min(a, b)
+		config['max_swap'] = math.max(a, b)
+
+		-- TODO: make this configurable
+		config['hk_complete'] = forms.gettext(hk_complete) or 'Ctrl+Shift+End'
+		config['completed_games'] = {}
+
+		-- internal information for output
+		config['frame_count'] = 0
+		config['total_swaps'] = 0
+		config['game_frame_count'] = {}
+		config['game_swaps'] = {}
+	end
+
+	function random_seed()
+		math.randomseed(os.time() + os.clock()*1000)
+		for i = 0, 1000 do math.random() end
+		return math.random(999999999)
+	end
+
+	function randomize_seed()
+		forms.settext(seed_text, random_seed())
+	end
+
+	form = forms.newform(340, 230, "Bizhawk Shuffler v2 Setup")
+
+	seed_text = forms.textbox(form, 0, 100, 20, "UNSIGNED", 10, 10)
+	forms.label(form, "Seed", 115, 13, 40, 20)
+	forms.settext(seed_text, config['seed'] or random_seed())
+	forms.button(form, "Randomize Seed", randomize_seed, 160, 10, 150, 20)
+
+	min_text = forms.textbox(form, 0, 40, 20, "UNSIGNED", 10, 40)
+	forms.label(form, "Minimum Swap Time (in seconds)", 55, 43, 200, 20)
+	forms.settext(min_text, config['min_swap'] or 15)
+
+	max_text = forms.textbox(form, 0, 40, 20, "UNSIGNED", 10, 70)
+	forms.label(form, "Maximum Swap Time (in seconds)", 55, 73, 200, 20)
+	forms.settext(max_text, config['max_swap'] or 45)
+
+	hk_complete = forms.dropdown(form, HOTKEY_OPTIONS, 10, 100, 150, 20)
+	forms.label(form, "Hotkey: Game Completed", 165, 103, 150, 20)
+	forms.settext(hk_complete, config['hk_complete'] or 'Ctrl+Shift+End')
+
+	forms.label(form, "Resuming a run?", 28, 134, 130, 20)
+	resume = forms.checkbox(form, "", 10, 130)
+	start_btn = forms.button(form, "Start New Shuffler", start_handler, 160, 130, 150, 20)
+
+	function toggle_resuming()
+		if forms.ischecked(resume) then
+			forms.settext(start_btn, "Resume Existing Shuffler")
+		else
+			forms.settext(start_btn, "Start New Shuffler")
+		end
+	end
+
+	forms.addclick(resume, toggle_resuming)
+	forms.label(form, "** Based on the original Bizhawk Shuffler by Brossentia", 10, 163, 300, 20)
+end
---
+++ shuffler.lua
@@ -0,0 +1,315 @@
+require 'shuffler-src.setupform'
+
+--[[
+	Bizhawk Shuffler 2 by authorblues, inspired by the original Bizhawk Shuffler by Brossentia
+	tested on Bizhawk v2.6.1 - http://tasvideos.org/BizHawk/ReleaseHistory.html
+	released under MIT License
+--]]
+
+-- database functions
+function d_get(k) return userdata.get('shuf_' .. k) end
+function d_set(k, v) userdata.set('shuf_' .. k, v) end
+
+config = {}
+next_swap_time = 0
+
+-- determine operating system for the purpose of commands
+_PLATFORMS = {['dll'] = 'WIN', ['so'] = 'LINUX', ['dylib'] = 'MAC'}
+PLATFORM = _PLATFORMS[package.cpath:match("%p[\\|/]?%p(%a+)")]
+
+GAMES_FOLDER = 'games'
+STATES_FOLDER = GAMES_FOLDER .. '/.savestates'
+
+-- folders needed for the shuffler to run
+os.execute('mkdir output-info')
+os.execute('mkdir "' .. GAMES_FOLDER .. '"')
+os.execute('mkdir "' .. STATES_FOLDER .. '"')
+
+-- loads primary config file
+function load_config(f)
+	local fn = loadfile(f)
+	if fn ~= nil then fn() end
+	return fn ~= nil
+end
+
+-- dump lua object
+function dump(o)
+	function _dump(o, a, b)
+		if type(o) == 'table' then
+			local s = ''
+			for k,v in pairs(o) do
+				if type(k) ~= 'number' then k = '"'..k..'"' end
+				s = s..a..'['..k..'] = '.._dump(v, '', '')..','..b
+			end
+			return '{'..b..s..'}'..b
+		elseif type(o) == 'string' then
+			return '"' .. o .. '"'
+		else
+			return tostring(o)
+		end
+	end
+
+	return _dump(o, "\t", "\n")
+end
+
+-- saves primary config file
+function save_config(config, f)
+	write_data(f, 'config=\n'..dump(config))
+end
+
+-- output data to files (for OBS support)
+function write_data(filename, data, mode)
+	local handle = io.open(filename, mode or 'w')
+	handle:write(data)
+	handle:close()
+end
+
+-- get list of games
+function get_games_list()
+	local cmd = 'ls ' .. GAMES_FOLDER .. ' > shuffler-src/games-list.txt'
+	if PLATFORM == 'WIN' then
+		cmd = 'dir ' .. GAMES_FOLDER .. ' /B > shuffler-src/games-list.txt'
+	end
+	os.execute(cmd)
+
+	local games = {}
+	local fp = io.open('shuffler-src/games-list.txt', 'r')
+	for x in fp:lines() do
+		if x ~= '.savestates' then
+			table.insert(games, x)
+		end
+	end
+	fp:close()
+
+	-- find .cue files and remove the associated bin/iso
+	for _,filename in ipairs(games) do
+		if ends_with(filename, '.cue') then
+			-- open the cue file, oh god here we go...
+			fp = io.open(GAMES_FOLDER .. '/' .. filename, 'r')
+			for line in fp:lines() do
+				-- look for the line that starts with FILE and remove the rest of the stuff
+				if starts_with(line, "FILE") and ends_with(line, "BINARY") then
+					local target = line:sub(7, -9)
+					-- if this file is listed in the file list, remove it
+					for i = #games, 1, -1 do
+						if ends_with(games[i], target) then
+							table.remove(games, i)
+						end
+					end
+					break
+				end
+			end
+			fp:close()
+		end
+	end
+
+	table_subtract(games, config['completed_games'])
+	return games
+end
+
+-- delete savestates folder
+function delete_savestates()
+	local cmd = 'rm -rf "' .. STATES_FOLDER .. '"'
+	if PLATFORM == 'WIN' then
+		cmd = 'rmdir "' .. STATES_FOLDER .. '" /S /Q'
+	end
+	os.execute(cmd)
+end
+
+function table_subtract(t2, t1)
+	local t = {}
+	for i = 1, #t1 do
+		t[t1[i]] = true
+	end
+	for i = #t2, 1, -1 do
+		if t[t2[i]] then
+			table.remove(t2, i)
+		end
+	end
+end
+
+function get_current_game()
+	return config['current_game'] or nil
+end
+
+function update_next_swap_time()
+	next_swap_time = config['frame_count'] +
+		math.random(config['min_swap'] * 60, config['max_swap'] * 60)
+end
+
+function save_current_game()
+	local g = get_current_game()
+	if g ~= nil then
+		savestate.save(STATES_FOLDER .. '/' .. g .. '.state')
+	end
+end
+
+function file_exists(f)
+	local p = io.open(f, 'r')
+	if p == nil then return false end
+	io.close(p)
+	return true
+end
+
+-- we don't load the savestate here because (for some unbelievably f***ed up reason),
+-- client.openrom() causes the whole script to reload, forcing us to use userdata
+-- storage to determine if this is the initial execution of the script, or a reload
+-- caused by openrom(). in any case, loading the savestate here seems to run into
+-- a race condition, so we load the savestate at the beginning of the reloaded script
+function load_game(g)
+	client.openrom(GAMES_FOLDER .. '/' .. g)
+end
+
+function get_next_game()
+	local prev = config['current_game'] or nil
+	local all_games = get_games_list()
+
+	-- remove the currently loaded game and see if there are any other options
+	table_subtract(all_games, { prev })
+	if #all_games == 0 then return prev end
+	return all_games[math.random(#all_games)]
+end
+
+-- save current game's savestate, backup config, and load new game
+function swap_game()
+	-- if the game isn't changing, stop here and just update the timer
+	-- (you might think we should just disable the timer at this point, but this
+	-- allows new games to be added mid-run without the timer being disabled)
+	local next_game = get_next_game()
+	if next_game == get_current_game() then
+		update_next_swap_time()
+		return
+	end
+
+	-- at this point, save the game and update the new "current" game after
+	save_current_game()
+	config['current_game'] = next_game
+
+	-- save an updated randomizer seed
+	config['nseed'] = math.random(9999999999)
+	save_config(config, 'shuffler-src/config.lua')
+
+	-- mute the sound for a moment to help with the swap
+	config['sound'] = client.GetSoundOn()
+	client.SetSoundOn(false)
+
+	-- load the new game WHICH IS JUST GOING TO RESTART THE WHOLE SCRIPT f***
+	load_game(get_current_game())
+end
+
+function starts_with(a, b)
+	return a:sub(1, #b) == b
+end
+
+function ends_with(a, b)
+	return a:sub(-#b) == b
+end
+
+function strip_ext(filename)
+	local ndx = filename:find("\.[^\.]*$")
+	return filename:sub(1, ndx-1)
+end
+
+-- this is going to be an APPROXIMATION and is not a substitute for an actual
+-- timer. games do not run at a consistent or exact 60 fps, so this method is
+-- provided purely for entertainment purposes
+function frames_to_time(f)
+	local sec = math.floor(f   / 60)
+	local min = math.floor(sec / 60)
+	local hrs = math.floor(min / 60)
+	return string.format('%02d:%02d:%02d', hrs, min%60, sec%60)
+end
+
+function mark_complete()
+	-- mark the game as complete in the config file rather than moving files around
+	table.insert(config['completed_games'], get_current_game())
+	print(get_current_game() .. ' marked complete')
+	if #get_games_list() == 0 then
+		-- the shuffler is complete!
+		print('Shuffler complete!')
+		d_set('running', false)
+	else
+		swap_game()
+	end
+end
+
+-- load primary configuration
+load_config('shuffler-src/config.lua')
+
+function complete_setup()
+	save_config(config, 'shuffler-src/config.lua')
+	math.randomseed(config['nseed'])
+
+	if config['frame_count'] == 0 then
+		print('deleting savestates!')
+		delete_savestates()
+	end
+
+	d_set('running', true)
+	swap_game()
+end
+
+if d_get('running') then
+	-- THIS CODE RUNS EVERY TIME THE SCRIPT RESTARTS
+	-- which is specifically after a call to client.openrom()
+
+	-- I will try to limit the number of comments I write solely to complain about
+	-- this design decision, but I make no promises.
+	local state = STATES_FOLDER .. '/' .. get_current_game() .. '.state'
+	if file_exists(state) then
+		savestate.load(state)
+	end
+
+	-- update swap counter for this game
+	local new_swaps = (config['game_swaps'][get_current_game()] or 0) + 1
+	config['game_swaps'][get_current_game()] = new_swaps
+	write_data('output-info/current-swaps.txt', new_swaps)
+
+	-- update total swap counter
+	config['total_swaps'] = (config['total_swaps'] or 0) + 1
+	write_data('output-info/total-swaps.txt', config['total_swaps'])
+
+	-- update game name
+	write_data('output-info/current-game.txt', strip_ext(get_current_game()))
+
+	update_next_swap_time()
+	client.SetSoundOn(config['sound'] or true)
+else
+	-- THIS CODE RUNS ONLY ON THE INITIAL SCRIPT SETUP
+	client.displaymessages(false)
+	d_set('running', false)
+	setup_form(complete_setup)
+end
+
+prev_input = input.get()
+frames_since_restart = 0
+while true do
+	if d_get('running') then
+		local frame_count = (config['frame_count'] or 0) + 1
+		config['frame_count'] = frame_count
+		frames_since_restart = frames_since_restart + 1
+
+		-- update the frame count specifically for the active game as well
+		local cgf = (config['game_frame_count'][get_current_game()] or 0) + 1
+		config['game_frame_count'][get_current_game()] = cgf
+
+		-- save time info to files for OBS display
+		write_data('output-info/total-time.txt', frames_to_time(frame_count))
+		write_data('output-info/current-time.txt', frames_to_time(cgf))
+
+		-- time to swap!
+		if frame_count >= next_swap_time then swap_game() end
+
+		-- calculate input "rises" by subtracting the previously held inputs from the inputs on this frame
+		local input_rise = input.get()
+		for k,v in pairs(prev_input) do input_rise[k] = nil end
+		prev_input = input.get()
+
+		-- mark the game as complete if the hotkey is pressed (and some time buffer)
+		-- the time buffer should hopefully prevent somebody from attempting to
+		-- press the hotkey and the game swapping, marking the wrong game complete
+		if input_rise[config['hk_complete']] and frames_since_restart > math.min(3, config['min_swap']/2) * 60 then mark_complete() end
+	end
+
+	emu.frameadvance()
+end
