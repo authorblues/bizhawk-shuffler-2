16ba4a677b405142e2b432a78e66f4cc6042ef29
authorblues <authorblues@gmail.com>
2021-07-06 16:26:02 -0400

    Initial commit of plugin boilerplate

---
+++ plugin.lua
@@ -0,0 +1,31 @@
+local plugin = {}
+
+plugin.name = ""
+plugin.author = ""
+plugin.settings = {}
+
+plugin.description =
+[[
+]]
+
+-- called once at the start
+function plugin.on_setup(data)
+end
+
+-- called each time a game/state loads
+function plugin.on_game_load(data)
+end
+
+-- called each frame
+function plugin.on_frame(data)
+end
+
+-- called each time a game/state is saved (before swap)
+function plugin.on_game_save(data)
+end
+
+-- called each time a game is marked complete
+function plugin.on_complete(data)
+end
+
+return plugin
