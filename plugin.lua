ae723033d25844407db3d2c4d38a376977785eda
authorblues <authorblues@gmail.com>
2021-07-06 20:44:43 -0400

    plugin.lua: Add settings param to methods

--- plugin.lua
+++ plugin.lua
@@ -9,23 +9,23 @@ plugin.description =
 ]]
 
 -- called once at the start
-function plugin.on_setup(data)
+function plugin.on_setup(data, settings)
 end
 
 -- called each time a game/state loads
-function plugin.on_game_load(data)
+function plugin.on_game_load(data, settings)
 end
 
 -- called each frame
-function plugin.on_frame(data)
+function plugin.on_frame(data, settings)
 end
 
 -- called each time a game/state is saved (before swap)
-function plugin.on_game_save(data)
+function plugin.on_game_save(data, settings)
 end
 
 -- called each time a game is marked complete
-function plugin.on_complete(data)
+function plugin.on_complete(data, settings)
 end
 
 return plugin
