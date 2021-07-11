068282dbb1aec70caa21c7d8342e20b4e17ac2a2
authorblues <authorblues@gmail.com>
2021-07-11 02:33:30 -0400

    Simplify Sonic ring plugin (no longer using it as an example)

--- plugins/sonic-sync-rings.lua
+++ plugins/sonic-sync-rings.lua
@@ -9,11 +9,6 @@ plugin.description =
 	This plugin is designed for Sonic 1, 2, and 3 in a shuffle. Ring counts should be synchronized across all three games.
 ]]
 
--- called once at the start
-function plugin.on_setup(data)
-end
-
--- called each time a game/state loads
 function plugin.on_game_load(data)
 	local gamestate = bit.band(mainmemory.readbyte(0xF600), 0x7F)
 	if gamestate == 0x0C then
@@ -22,21 +17,11 @@ function plugin.on_game_load(data)
 	end
 end
 
--- called each frame
 function plugin.on_frame(data)
 	local gamestate = bit.band(mainmemory.readbyte(0xF600), 0x7F)
 	if gamestate == 0x0C then
 		data['rings'] = mainmemory.read_s16_be(0xFE20)
 	end
-end
-
--- called each time a game/state is saved (before swap)
-function plugin.on_game_save(data)
-	print('saving ' .. tostring(data['rings']) .. ' rings')
-end
-
--- called each time a game is marked complete
-function plugin.on_complete(data)
 end
 
 return plugin
