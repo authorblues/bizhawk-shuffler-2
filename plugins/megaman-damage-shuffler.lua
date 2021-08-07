18c9bbc3bbfa482ddba63dac3cd1b861e7cadb93
kalimag <kalimag@users.noreply.github.com>
2021-08-07 17:25:26 +0200

    MMDS: Add a number of bootleg games

--- plugins/megaman-damage-shuffler.lua
+++ plugins/megaman-damage-shuffler.lua
@@ -451,6 +451,31 @@ local gamedata = {
 		is_battle = function() return mainmemory.read_u8(0x0CCAD4) == 1 end,
 		func=super_adventure_rockman_swap,
 	},
+	['zook-hero'] = {
+		gethp=function() return memory.read_u8(0x52, "HRAM") end,
+		getlc=function() return memory.read_u8(0x60, "HRAM") end,
+		maxhp=function() return 20 end,
+	},
+	['zook-man-zx4'] = {
+		gethp=function() return memory.read_u8(0x1638, "IWRAM") end,
+		getlc=function() return memory.read_u8(0x1634, "IWRAM") end,
+		maxhp=function() return 11 end,
+	},
+	['rockman-and-crystal'] = {
+		gethp=function() return memory.read_u8(0x163C, "IWRAM") end,
+		getlc=function() return memory.read_u8(0x1638, "IWRAM") end,
+		maxhp=function() return 11 end,
+	},
+	['rocman-x-gb'] = {
+		gethp=function() return memory.read_u8(0x025B, "WRAM") end,
+		getlc=function() return memory.read_u8(0x5F, "HRAM") end,
+		maxhp=function() return 8 end,
+	},
+	['rockman-8-gb'] = {
+		gethp=function() return memory.read_u8(0x027C, "WRAM") end,
+		getlc=function() return memory.read_u8(0x025E, "WRAM") end,
+		maxhp=function() return 8 end,
+	},
 }
 
 local backupchecks = {
