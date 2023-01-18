2da9ea357c21066563fe68e21b36ddfa75ebd11f
kalimag <kalimag@users.noreply.github.com>
2023-01-18 09:55:06 +0100

    Countdown plugin: add font size option and border

--- plugins/countdown.lua
+++ plugins/countdown.lua
@@ -6,6 +6,7 @@ plugin.minversion = "2.6.2"
 plugin.settings =
 {
 	{ name='threshold', type='number', label='Threshold (in seconds)', default=3 },
+	{ name='fontsize', type='number', datatype='UNSIGNED', label='Font Size', default=12 },
 }
 
 plugin.description =
@@ -17,7 +18,7 @@ function plugin.on_frame(data, settings)
 	local seconds = math.ceil((next_swap_time - config.frame_count) / 60)
 	if seconds <= settings.threshold then
 		gui.use_surface('client')
-		gui.drawText(10, 10, string.format("Swapping in %d...", seconds))
+		gui.drawText(10, 10, string.format("Swapping in %d...", seconds), 0xFFFFFFFF, 0xFF000000, settings.fontsize or 12)
 	end
 end
 
